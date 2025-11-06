9223df893cf832b0b11cc501e308a505c1386a6a
Akshay Kumar Shaw <akshaykumarshaw@Akshays-MacBook-Air.local>
2025-11-06 19:57:15 +0530

    Pushing full project.

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+
+function App() {
+  let [color,setColor]=useState("bg-amber-600")
+  return (
+    <div className={`h-screen w-screen flex flex-wrap justify-center items-end ${color}`}>
+      <div className="backdrop-blur-md bg-white/40 border border-white/30 mb-15 rounded-3xl flex flex-wrap gap-4 p-3">
+        <button className="bg-red-600 text-white py-2 px-4 shadow-2xl rounded-2xl" onClick={()=>{setColor("bg-red-600")}}>Red</button>
+        <button className="bg-green-600 text-white py-2 px-4 shadow-2xl rounded-2xl" onClick={()=>{setColor("bg-green-600")}}>Green</button>
+        <button className="bg-yellow-500 text-black py-2 px-4 shadow-2xl rounded-2xl" onClick={()=>{setColor("bg-yellow-500")}}>Yellow</button>
+        <button className="bg-blue-600 text-white py-2 px-4 shadow-2xl rounded-2xl"onClick={()=>{setColor("bg-blue-600")}}>Blue</button>
+        <button className="bg-purple-600 text-white py-2 px-4 shadow-2xl rounded-2xl"onClick={()=>{setColor("bg-purple-600")}}>Purple</button>
+        <button className="bg-pink-500 text-white py-2 px-4 shadow-2xl rounded-2xl" onClick={()=>{setColor("bg-pink-500")}}>Pink</button>
+        <button className="bg-orange-500 text-white py-2 px-4 shadow-2xl rounded-2xl" onClick={()=>{setColor("bg-orange-500")}}>Orange</button>
+        <button className="bg-teal-500 text-white py-2 px-4 shadow-2xl rounded-2xl" onClick={()=>{setColor("bg-teal-500")}}>Teal</button>
+        <button className="bg-gray-600 text-white py-2 px-4 shadow-2xl rounded-2xl" onClick={()=>{setColor("bg-gray-600")}}>Gray</button>
+        <button className="bg-lime-500 text-black py-2 px-4 shadow-2xl rounded-2xl" onClick={()=>{setColor("bg-lime-500")}}>Lime</button>
+        <button className="bg-indigo-600 text-white py-2 px-4 shadow-2xl rounded-2xl" onClick={()=>{setColor("bg-indigo-600")}}>Indigo</button>
+        <button className="bg-cyan-500 text-white py-2 px-4 shadow-2xl rounded-2xl" onClick={()=>{setColor("bg-cyan-500")}}>Cyan</button>
+        <button className="bg-amber-600 text-white py-2 px-4 shadow-2xl rounded-2xl" onClick={()=>{setColor("bg-amber-600")}}>Amber</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
